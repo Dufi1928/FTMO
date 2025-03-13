18675f91b37fc005b5cfcbb760ce8b80cb6278b1
Ivan <83720387+Dufi1928@users.noreply.github.com>
2025-03-13 14:47:00 +0100

    API Done

--- matches/views.py
+++ matches/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Match, MatchSet
 from .serializers import MatchSerializer, MatchSetSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class MatchViewSet(viewsets.ModelViewSet):
     queryset = Match.objects.all()
     serializer_class = MatchSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class MatchSetViewSet(viewsets.ModelViewSet):
     queryset = MatchSet.objects.all()
     serializer_class = MatchSetSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
--- teams/views.py
+++ teams/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Team, Player
 from .serializers import TeamSerializer, PlayerSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class PlayerViewSet(viewsets.ModelViewSet):
     queryset = Player.objects.all()
     serializer_class = PlayerSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
